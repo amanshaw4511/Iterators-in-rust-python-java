2f08a679fd9ba2d51e0a43267f1f7c2bcc7bc192
aman shaw <amansahw4511@gmail.com>
2021-05-14 10:47:02 +0530

    java iterator use example
    java Iterator uses example

--- java/Iterator.java
+++ java/Iterator.java
@@ -1,24 +1,74 @@
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 
 public class Iterator {
     public static void main(String... args) {
-        var num = new Digits(14524);
-        // StreamSupport.stream(num.spliterator(), false).forEach(System.out::println);
-        var count_digit = StreamSupport.stream(num.spliterator(), false).count();
-        System.out.println(count_digit);
-        // for (int digit : num) {
-        //     System.out.println(digit);
-        // }
+        // loop through for loop 
+        for (int digit : new Digits(14562)) {
+            System.out.print(digit + " ");
+        }
+        println("");
+
+        // collect digits in list
+        List<Integer> digit_list = getStream(new Digits(14562)).collect(Collectors.toList());
+        println("digit_list: " + digit_list);
+
+        // count digit, max and min of digits
+        int digit_count = (int)getStream(new Digits(14562)).count();
+        int digit_max = getStream(new Digits(14562)).max((a,b) -> a-b).get();
+        int digit_min = getStream(new Digits(14562)).min((a,b) -> a-b).get();
+        println("digit_count: " + digit_count);
+        println("digit_max: " + digit_max);
+        println("digit_min: " + digit_min);
+
+        // sum and product of digits 
+        int digit_sum = getStream(new Digits(14562)).reduce(0, (a,x) -> a + x);
+        int digit_product = getStream(new Digits(14562)).reduce(1, (a,x) -> a * x);
+        println("digit_sum: " + digit_sum);
+        println("digit_product: " + digit_product);
+
+        // abs differnce between sum of even digit and sum of odd digit
+        int digit_even_sum = getStream(new Digits(14562))
+                                .filter(e -> e % 2 == 0)
+                                .reduce(0, (a,x) -> a + x);
+        int digit_odd_sum = getStream(new Digits(14562))
+                                .filter(e -> e % 2 != 0)
+                                .reduce(0, (a,x) -> a + x);
+        int diff = Math.abs(digit_even_sum - digit_odd_sum);
+        println("digit_abs_diff_even_odd: " + diff);
+
+        // number in sorted digit
+        int digit_sorted = getStream(new Digits(14562))
+                                .sorted()
+                                .reduce(0, (a,x) -> (a * 10) + x);
+        println("digit_sorted: " + digit_sorted);
+
+    }
+
+
+    public static Stream<Integer> getStream(Iterable<Integer> iterable) {
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
+
+    public static void println(Object obj) {
+        System.out.println(obj);
     }
 }
 
+
 class Digits implements Iterable<Integer> {
-    int num;
+    private int num;
 
     Digits(int num) {
         this.num = num;
+    }
+
+    public int getNumber() {
+        return this.num;
     }
 
 	@Override
